f99671121e1334622f87220d10fb19071f9c6a48
mjsterck <sterckx.matthias@live.com>
2019-10-04 12:36:20 -0400

    Adds all functions to arrays.c

--- PROG2/arrays.c
+++ PROG2/arrays.c
@@ -1,4 +1,76 @@
-//
-// Created by sterc on 10/4/2019.
-//
+double getPositiveAverage(double myArray[], int numItems)
+{
+    if (numItems <= 0) return 0;
+    double sum = 0;
+    for (int i = 0; i < numItems; i++)
+    {
+        sum += myArray[i];
+    }
+    return sum / numItems;
+}
 
+int countRangeValues(double myArray[], int numItems, double toCount)
+{
+    int count = 0;
+    double lowerBound = toCount - 0.5;
+    double upperBound = toCount + 0.5;
+    for (int i = 0; i < numItems; i++)
+    {
+        if (myArray[i] >= lowerBound && myArray[i] < upperBound) count++;
+    }
+    return count;
+}
+
+double getMaxAbsolute(double myArray[], int numItems)
+{
+    double max = 0;
+    double maxAbsolute = 0;
+    for (int i = 0; i < numItems; i++)
+    {
+        if (myArray[i] < 0)
+        {
+            if (-myArray[i] > maxAbsolute)
+            {
+                max = myArray[i];
+                maxAbsolute = -max;
+            }
+        }
+        else
+        {
+            if (myArray[i] >= maxAbsolute)
+            {
+                max = maxAbsolute = myArray[i];
+            }
+        }
+    }
+    return max;
+}
+
+int countInverses(int myArray[], int numItems)
+{
+    int pairs = 0;
+    int arrayCopy[numItems];
+    for (int i = 0; i < numItems; i++)
+    {
+        arrayCopy[i] = myArray[i];
+    }
+    for (int i = 0; i < numItems; i++)
+    {
+        for (int j = i + 1; j < numItems; j++)
+        {
+            if (arrayCopy[i] != 0 && arrayCopy[j] != 0 && arrayCopy[i] + arrayCopy[j] == 0)
+            {
+                pairs++;
+                arrayCopy[i] = 0;
+                arrayCopy[j] = 0;
+            }
+        }
+    }
+    return pairs;
+}
+
+int getMaxCount(double myArray[], int numItems)
+{
+    double maxValue = getMaxAbsolute(myArray, numItems);
+    return countRangeValues(myArray, numItems, maxValue);
+}
